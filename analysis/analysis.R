2b6356f5b7c32a14152257cf971dfdf7994b6474
Blake Blackwell <blake.blackwell@chk.com>
2018-08-05 15:12:11 -0500

    Fixed error with duplicate records in growing and shrinking datasets

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -7,37 +7,21 @@ library(readxl)
 library(dplyr)
 library(ggplot2)
 
-all_data <- read_excel("data/church_stats.XLSX")
+full_dataset <- read_xlsx("data/church_stats.XLSX")
 
-cols <- c("YEAR", "STATEAB", "GRPCODE", "GRPNAME", "ADHERENT")
-
-all_state_data <- all_data[cols]
-
-denom_data <- all_state_data %>% 
-                group_by(GRPNAME, YEAR) %>% 
-                summarize(TOTAL = sum(ADHERENT)) %>%
-                mutate(FIRST_YEAR = first(YEAR), 
-                       FIRST_YEAR_TOTAL = first(TOTAL), 
-                       DIFFERENCE_FROM_FIRST = TOTAL - FIRST_YEAR_TOTAL,
-                       PERCENT_DIFFERENCE_FROM_FIRST = DIFFERENCE_FROM_FIRST / FIRST_YEAR_TOTAL * 100) %>%
-                filter(FIRST_YEAR == 1980, FIRST_YEAR_TOTAL > 0) %>%
-                filter(YEAR == 2010, PERCENT_DIFFERENCE_FROM_FIRST != 0, PERCENT_DIFFERENCE_FROM_FIRST != -100) %>%
-                arrange(PERCENT_DIFFERENCE_FROM_FIRST)
-
-
+#minimize dataset
 dataset <- full_dataset[c("YEAR", "STATEAB", "GRPCODE", "GRPNAME", "ADHERENT")] %>%
-  group_by(GRPNAME, YEAR) %>%
+  group_by(GRPNAME, YEAR) %>% 
   summarize(TOTAL = sum(ADHERENT)) %>%
   mutate(FIRST_YEAR = first(YEAR), 
-         FIRST_YEAR_TOTAL = first(TOTAL),
+         FIRST_YEAR_TOTAL = first(TOTAL), 
+         LAST_YEAR = last(YEAR),
+         LAST_YEAR_TOTAL = last(TOTAL),
          DIFFERENCE_FROM_FIRST = TOTAL - FIRST_YEAR_TOTAL,
-         PERCENT_DIFFERENCE_FROM_FIRST = DIFFERENCE_FROM_FIRST / FIRST_YEAR_TOTAL * 100
-  ) %>%
-  filter(FIRST_YEAR == 1980, 
-         FIRST_YEAR_TOTAL > 0) %>%
-  filter(YEAR == 2010,
-         PERCENT_DIFFERENCE_FROM_FIRST != 0, 
-         PERCENT_DIFFERENCE_FROM_FIRST != -100) %>%
+         PERCENT_DIFFERENCE_FROM_FIRST = DIFFERENCE_FROM_FIRST / FIRST_YEAR_TOTAL * 100) %>%
+  filter(YEAR == 2010, FIRST_YEAR == 1980, FIRST_YEAR_TOTAL > 0, LAST_YEAR == 2010, LAST_YEAR_TOTAL > 0, PERCENT_DIFFERENCE_FROM_FIRST != -100) %>%
   arrange(PERCENT_DIFFERENCE_FROM_FIRST)
-                
-View(dataset)
+
+current_year_dataset <- dataset %>% filter(YEAR == max(dataset$YEAR))
+
+View(head(current_year_dataset, 10))
--- server.R
+++ server.R
@@ -16,8 +16,7 @@ function(input, output) {
                        LAST_YEAR_TOTAL = last(TOTAL),
                        DIFFERENCE_FROM_FIRST = TOTAL - FIRST_YEAR_TOTAL,
                        PERCENT_DIFFERENCE_FROM_FIRST = DIFFERENCE_FROM_FIRST / FIRST_YEAR_TOTAL * 100) %>%
-                filter(FIRST_YEAR == 1980, FIRST_YEAR_TOTAL > 0, LAST_YEAR == 2010, LAST_YEAR_TOTAL > 0, PERCENT_DIFFERENCE_FROM_FIRST != -100) %>%
-                arrange(PERCENT_DIFFERENCE_FROM_FIRST)
+                filter(FIRST_YEAR == 1980, FIRST_YEAR_TOTAL > 0, LAST_YEAR == 2010, LAST_YEAR_TOTAL > 0, PERCENT_DIFFERENCE_FROM_FIRST != -100)
             
   
   output$denomination_selector <- renderUI({
@@ -30,15 +29,17 @@ function(input, output) {
 
   disp_cols <- c("GRPNAME", "FIRST_YEAR_TOTAL", "LAST_YEAR_TOTAL", "PERCENT_DIFFERENCE_FROM_FIRST")
   
+  current_year_dataset <- dataset %>% filter(YEAR == max(dataset$YEAR))
+  
   #create table based on shrinking denoms
   output$shrinking_denoms <- renderTable({
-    head(dataset[disp_cols], 10)
+    head(current_year_dataset[disp_cols] %>% arrange(PERCENT_DIFFERENCE_FROM_FIRST), 10)
   })
   
   #create table based on growing denoms
   output$growing_denoms <- renderTable({
     
-    head(dataset[disp_cols] %>% 
+    head(current_year_dataset[disp_cols] %>% 
            filter(PERCENT_DIFFERENCE_FROM_FIRST > 0) %>% 
            arrange(desc(PERCENT_DIFFERENCE_FROM_FIRST)), 
          10)
@@ -47,11 +48,7 @@ function(input, output) {
     
   #Create plot based off of input
   output$church_stats <- renderPlot({ 
-    
       ggplot(denom_data(), aes(x=YEAR, y=TOTAL)) + geom_bar(stat="identity", fill="steelblue")
       
   })
-  
-  
-  
 }
